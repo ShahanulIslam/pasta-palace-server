43f4a1303fdc99d51245527d88cf9e61d25ec2fd
Your Name <shahanricky86@gmail.com>
2023-05-02 21:50:31 +0600

    chef data

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app =express();
 const port =5000;
+const chef = require("./data/chefs.json")
 
 app.get('/', (req, res) => {
     res.send("Chef is running")
+  })
+app.get('/chef', (req, res) => {
+    res.send(chef)
   })
   
   app.listen(port, () => {
