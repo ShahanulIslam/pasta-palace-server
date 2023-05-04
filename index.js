1ce2faf44819c04941927dc6d98b7247e8afc3e5
Your Name <shahanricky86@gmail.com>
2023-05-04 22:05:04 +0600

    add-json

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express");
-const app =express();
-const port =5000;
+const app = express();
+const port = 5000;
 const cors = require("cors")
 const chef = require("./data/chefs.json")
+const foods = require("./data/foods.json")
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send("Chef is running")
-  })
+  res.send("Chef is running")
+})
 app.get('/chef', (req, res) => {
-    res.send(chef)
-  })
-  app.get('/chef/:id', (req, res) => {
-    const id =  req.params.id;
-    const chefDetails = chef.find(c => c.id == id);
-    res.send(chefDetails);
-  })
-  
-  app.listen(port, () => {
-    console.log(`Chef recipe hunter server running on port ${port}`)
-  })
+  res.send(chef)
+})
+app.get('/foods', (req, res) => {
+  res.send(foods)
+})
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  const chefDetails = chef.find(c => c.id == id);
+  res.send(chefDetails);
+})
+
+app.listen(port, () => {
+  console.log(`Chef recipe hunter server running on port ${port}`)
+})
